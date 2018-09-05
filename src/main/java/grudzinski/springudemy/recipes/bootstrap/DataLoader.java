9ceb51d8c5628aecdf30cc8abfeb14d6dcb1132f
Karol Grudziński <krlgrudzinski@gmail.com>
2018-09-05 21:53:38 +0200

    Added second recipe to database initialization

--- src/main/java/grudzinski/springudemy/recipes/bootstrap/DataLoader.java
+++ src/main/java/grudzinski/springudemy/recipes/bootstrap/DataLoader.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
@@ -39,6 +40,29 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
         Category mexicanCategory = categoryRepository.findByDescription("Mexican").get();
         Category veganCategory = categoryRepository.findByDescription("Vegan").get();
 
+        Optional<UnitOfMeasure> eachUnitOptional = unitOfMeasureRepository.findByDescription("Each");
+        if(!eachUnitOptional.isPresent()){
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+        Optional<UnitOfMeasure> cupsUnitOptional = unitOfMeasureRepository.findByDescription("Cup");
+        if(!cupsUnitOptional.isPresent()){
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+        Optional<UnitOfMeasure> litreUnitOptional = unitOfMeasureRepository.findByDescription("Litre");
+        if(!litreUnitOptional.isPresent()){
+            throw new RuntimeException("Expected UOM Not Found");
+        }
+        UnitOfMeasure eachUnit = eachUnitOptional.get();
+        UnitOfMeasure cupsUnit = cupsUnitOptional.get();
+        UnitOfMeasure litreUnit = litreUnitOptional.get();
+
+        Optional<Category> americanCategoryOptional = categoryRepository.findByDescription("American");
+        if(!americanCategoryOptional.isPresent()){
+            throw new RuntimeException("Expected Category Not Found");
+        }
+        Category americanCategory = americanCategoryOptional.get();
+
+
         // https://www.simplyrecipes.com/recipes/perfect_guacamole/
         Recipe guacamole = new Recipe();
         guacamole.setDescription("Perfect guacamole");
@@ -54,6 +78,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
         Ingredient avocado = new Ingredient();
         avocado.setDescription("ripe avocado");
         avocado.setAmount(BigDecimal.valueOf(2));
+        avocado.setUom(eachUnit);
         avocado.setRecipe(guacamole);
         guacamole.getIngredients().add(avocado);
 
@@ -85,6 +110,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
         Ingredient chiles = new Ingredient();
         chiles.setDescription("serrano chiles, stems and seeds removed, minced");
         chiles.setAmount(BigDecimal.valueOf(2));
+        chiles.setUom(eachUnit);
         chiles.setRecipe(guacamole);
         guacamole.getIngredients().add(chiles);
 
@@ -107,6 +133,7 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
         Ingredient tomato = new Ingredient();
         tomato.setDescription("ripe tomato, seeds and pulp removed, chopped");
         tomato.setAmount(BigDecimal.valueOf(0.5));
+        tomato.setUom(eachUnit);
         tomato.setRecipe(guacamole);
         guacamole.getIngredients().add(tomato);
 
@@ -150,6 +177,66 @@ public class DataLoader implements ApplicationListener<ContextRefreshedEvent> {
                 "To extend a limited supply of avocados, add either sour cream or cottage cheese to your guacamole dip. Purists may be horrified, but so what? It tastes great."));
 
         recipes.add(guacamole);
+
+
+        // Tacos
+
+        Recipe tacosRecipe = new Recipe();
+        tacosRecipe.setDescription("Spicy Grilled Chicken Taco");
+        tacosRecipe.setCookTime(9);
+        tacosRecipe.setPrepTime(20);
+        tacosRecipe.setDifficulty(Difficulty.MODERATE);
+
+        tacosRecipe.setDirections("1 Prepare a gas or charcoal grill for medium-high, direct heat.\n" +
+                "2 Make the marinade and coat the chicken: In a large bowl, stir together the chili powder, oregano, cumin, sugar, salt, garlic and orange zest. Stir in the orange juice and olive oil to make a loose paste. Add the chicken to the bowl and toss to coat all over.\n" +
+                "Set aside to marinate while the grill heats and you prepare the rest of the toppings.\n" +
+                "\n" +
+                "\n" +
+                "3 Grill the chicken: Grill the chicken for 3 to 4 minutes per side, or until a thermometer inserted into the thickest part of the meat registers 165F. Transfer to a plate and rest for 5 minutes.\n" +
+                "4 Warm the tortillas: Place each tortilla on the grill or on a hot, dry skillet over medium-high heat. As soon as you see pockets of the air start to puff up in the tortilla, turn it with tongs and heat for a few seconds on the other side.\n" +
+                "Wrap warmed tortillas in a tea towel to keep them warm until serving.\n" +
+                "5 Assemble the tacos: Slice the chicken into strips. On each tortilla, place a small handful of arugula. Top with chicken slices, sliced avocado, radishes, tomatoes, and onion slices. Drizzle with the thinned sour cream. Serve with lime wedges.\n" +
+                "\n" +
+                "\n" +
+                "Read more: http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/#ixzz4jvtrAnNm");
+
+        Notes tacoNotes = new Notes();
+        tacoNotes.setRecipeNotes("We have a family motto and it is this: Everything goes better in a tortilla.\n" +
+                "Any and every kind of leftover can go inside a warm tortilla, usually with a healthy dose of pickled jalapenos. I can always sniff out a late-night snacker when the aroma of tortillas heating in a hot pan on the stove comes wafting through the house.\n" +
+                "Today’s tacos are more purposeful – a deliberate meal instead of a secretive midnight snack!\n" +
+                "First, I marinate the chicken briefly in a spicy paste of ancho chile powder, oregano, cumin, and sweet orange juice while the grill is heating. You can also use this time to prepare the taco toppings.\n" +
+                "Grill the chicken, then let it rest while you warm the tortillas. Now you are ready to assemble the tacos and dig in. The whole meal comes together in about 30 minutes!\n" +
+                "\n" +
+                "\n" +
+                "Read more: http://www.simplyrecipes.com/recipes/spicy_grilled_chicken_tacos/#ixzz4jvu7Q0MJ");
+        tacoNotes.setRecipe(tacosRecipe);
+        tacosRecipe.setNotes(tacoNotes);
+
+
+        tacosRecipe.getIngredients().add(new Ingredient("Ancho Chili Powder", new BigDecimal(2), tableSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Dried Oregano", new BigDecimal(1), teaSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Dried Cumin", new BigDecimal(1), teaSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Sugar", new BigDecimal(1), teaSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Salt", new BigDecimal(".5"), teaSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Clove of Garlic, Choppedr", new BigDecimal(1), eachUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("finely grated orange zestr", new BigDecimal(1), tableSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("fresh-squeezed orange juice", new BigDecimal(3), tableSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Olive Oil", new BigDecimal(2), tableSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("boneless chicken thighs", new BigDecimal(4), tableSpoonUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("small corn tortillasr", new BigDecimal(8), eachUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("packed baby arugula", new BigDecimal(3), cupsUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("medium ripe avocados, slic", new BigDecimal(2), eachUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("radishes, thinly sliced", new BigDecimal(4), eachUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("cherry tomatoes, halved", new BigDecimal(".25"), litreUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("red onion, thinly sliced", new BigDecimal(".25"), eachUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("Roughly chopped cilantro", new BigDecimal(4), eachUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("cup sour cream thinned with 1/4 cup milk", new BigDecimal(4), cupsUnit, tacosRecipe));
+        tacosRecipe.getIngredients().add(new Ingredient("lime, cut into wedges", new BigDecimal(4), eachUnit, tacosRecipe));
+
+        tacosRecipe.getCategories().add(americanCategory);
+        tacosRecipe.getCategories().add(mexicanCategory);
+
+        recipes.add(tacosRecipe);
 
         return recipes;
     }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -5,10 +5,12 @@ INSERT INTO category (description) VALUES ('Mexican');
 INSERT INTO category (description) VALUES ('Fast food');
 INSERT INTO category (description) VALUES ('Vegan');
 
+INSERT INTO unit_of_measure (description) VALUES ('Each');
 INSERT INTO unit_of_measure (description) VALUES ('Teaspoon');
 INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (description) VALUES ('Cup');
 INSERT INTO unit_of_measure (description) VALUES ('Pinch');
 INSERT INTO unit_of_measure (description) VALUES ('Dash');
-INSERT INTO unit_of_measure (description) VALUES ('Grams');
-INSERT INTO unit_of_measure (description) VALUES ('Millilitres');
+INSERT INTO unit_of_measure (description) VALUES ('Gram');
+INSERT INTO unit_of_measure (description) VALUES ('Litre');
+INSERT INTO unit_of_measure (description) VALUES ('Millilitre');
