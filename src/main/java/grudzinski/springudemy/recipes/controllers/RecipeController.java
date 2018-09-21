2d0a3fb88bd301a8eee4214cd1229baa55e38194
Karol Grudziński <krlgrudzinski@gmail.com>
2018-09-21 18:27:30 +0200

    Added new view and controller methods for creating new Recipes

--- src/main/java/grudzinski/springudemy/recipes/controllers/RecipeController.java
+++ src/main/java/grudzinski/springudemy/recipes/controllers/RecipeController.java
@@ -1,11 +1,14 @@
 package grudzinski.springudemy.recipes.controllers;
 
+import grudzinski.springudemy.recipes.commands.RecipeCommand;
 import grudzinski.springudemy.recipes.services.RecipeService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Slf4j
 @Controller
@@ -21,5 +24,19 @@ public class RecipeController {
         model.addAttribute(recipeService.findById(new Long(id)));
         
         return "recipe/show";
+    }
+
+    @RequestMapping("/recipe/new")
+    public String newRecipe(Model model) {
+        model.addAttribute("recipe", new RecipeCommand());
+
+        return "recipe/recipeform";
+    }
+
+    @RequestMapping(value = "/recipe", method = RequestMethod.POST)
+    public String saveOrUpdate(@ModelAttribute RecipeCommand recipeCommand) {
+        RecipeCommand savedCommand = recipeService.saveRecipeCommand(recipeCommand);
+
+        return "redirect:/recipe/show/" + savedCommand.getId();
     }
 }
