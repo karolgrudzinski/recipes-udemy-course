10c961571b79188b402fe9f9cd20c610a892cd4b
Karol Grudziński <krlgrudzinski@gmail.com>
2018-09-22 00:08:36 +0200

    Mock repository not service in RecipeServiceImplTest.getRecipes()
    Former version stubbed recipeService.getRecipies(), but it ought to stub
    the repository.findall() since this is a test for the service.

--- src/test/java/grudzinski/springudemy/recipes/services/RecipeServiceImplTest.java
+++ src/test/java/grudzinski/springudemy/recipes/services/RecipeServiceImplTest.java
@@ -57,11 +57,13 @@ public class RecipeServiceImplTest {
         HashSet recipesData = new HashSet();
         recipesData.add(recipe);
 
-        when(recipeService.getRecipes()).thenReturn(recipesData);
+//        when(recipeService.getRecipes()).thenReturn(recipesData);
+        when(recipeRepository.findAll()).thenReturn(recipesData);
 
         Set<Recipe> recipes = recipeService.getRecipes();
 
         assertEquals(recipes.size(), 1);
         verify(recipeRepository, times(1)).findAll();
+        verify(recipeRepository, never()).findById(anyLong());
     }
 }
